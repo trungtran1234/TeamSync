bd9ede93d007f48ea725320dc5e70ab6903dab1c
Hasnain Mucklai <mucklaihasnain@gmail.com>
2025-01-06 17:06:14 -0500

    updated zoom link

--- teamSync-app/src/forms/SignIn.tsx
+++ teamSync-app/src/forms/SignIn.tsx
@@ -124,7 +124,7 @@ const SignIn: React.FC = () => {
             <button
               type="button"
               className="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
-              onClick={() => (window.location.href = 'https://new-subdomain.ngrok-free.app/auth')} 
+              onClick={() => (window.location.href = 'https://zoom.us/oauth/authorize?response_type=code&client_id=TUNGtIReTpqReWOgVyQieQ&redirect_uri=https://bullfrog-ample-routinely.ngrok-free.app/oauth/callback')} 
             >
               {/* Zoom Logo or Icon */}
               <img src={zoomIcon} alt="Zoom" className="w-6 h-6 mr-2" />
