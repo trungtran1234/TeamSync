151593545706779273c350e5f556207a027aeeeb
trungtrandev <trungtran63470@gmail.com>
2025-04-27 20:55:40 -0700

    remove stringtostream()

--- server/server.js
+++ server/server.js
@@ -559,15 +559,6 @@ app.post("/zoom-webhook", async (req, res) => {
   }
 });
 
-// Helper function to convert stream to string
-function streamToString(stream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on('data', (chunk) => chunks.push(chunk));
-    stream.on('error', reject);
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
